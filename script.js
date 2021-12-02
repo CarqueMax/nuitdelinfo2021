b190b323af3120a74c1459c32a2d2ab101881db8
Enzo <nzomorvan@gmail.com>
2021-12-02 21:20:40 +0100

    Première version du site :)

---
+++ script.js
@@ -0,0 +1,19 @@
+let wave1 = document.getElementById('wave1');
+let wave2 = document.getElementById('wave2');
+let wave3 = document.getElementById('wave3');
+let wave4 = document.getElementById('wave4');
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    wave1.style.backgroundPositionX = 40 + value * 0.3 + 'vw';
+    wave2.style.backgroundPositionX = 30 + value * -0.3 + 'vw';
+    wave3.style.backgroundPositionX = 20 + value * 0.1 + 'vw';
+    wave4.style.backgroundPositionX = 10 + value * -0.1 + 'vw';
+
+})
