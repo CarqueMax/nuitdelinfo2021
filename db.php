afc6334c756a231e0d391255cbd005c68bb8b294
Enzo <nzomorvan@gmail.com>
2021-12-03 07:36:34 +0100

    root enlever

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
   try
   {
   	
-  	$bdd = new PDO('mysql:host=localhost;dbname=website;charset=utf8', 'root','root',
+  	$bdd = new PDO('mysql:host=localhost;dbname=website;charset=utf8', 'root','',
   	array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
   }
   catch(Exception $e)
--- login_final.php
+++ login_final.php
@@ -4,7 +4,7 @@
     try
     {
         
-        $bdd = new PDO('mysql:host=localhost;dbname=website;charset=utf8', 'root','root',
+        $bdd = new PDO('mysql:host=localhost;dbname=website;charset=utf8', 'root','',
         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
     catch(Exception $e)
